e250760bd3c85ad33a382a68028402626273957f
jhendric98 <jhendric98@gmail.com>
2014-04-19 09:51:00 -0500

    Working on workflow.

--- ProcessFiles.R
+++ ProcessFiles.R
@@ -3,4 +3,17 @@
 #
 
 
+# import raw data
+
+# add column names
+
+# Process the features file
+#   - missing data
+#   - add correct holiday weeks
+
+#  Create random subspace files
+
+# Run model 
+
+# Cross validate results.
 
